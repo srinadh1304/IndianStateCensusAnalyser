79a540851dbb1174961d97c252eb2cdc7cb2acdf
srinadh1304 <srinadh1304@gmail.com>
2021-09-21 22:28:12 +0530

    ADDED: class to load to csv states data

---
+++ lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/CSVStates.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.indianstatecensusanalyser;
+import com.opencsv.bean.CsvBindByName;
+public class CSVStates 
+{
+	    @CsvBindByName(column = "SrNo", required = true)
+	    public int SrNo;
+	    @CsvBindByName(column = "State Name", required = true)
+	    public String state;
+	    @CsvBindByName(column = "TIN", required = true)
+	    public int TIN;
+	    @CsvBindByName(column = "StateCode", required = true)
+	    public String stateCode;
+	    @Override
+	    public String toString() 
+	    {
+	        return "IndiaStateCSV{" +
+	                "SrNo=" + SrNo +
+	                ", state='" + state + '\'' +
+	                ", TIN=" + TIN +
+	                ", stateCode='" + stateCode + '\'' +
+	                '}';
+	    }   
+}
