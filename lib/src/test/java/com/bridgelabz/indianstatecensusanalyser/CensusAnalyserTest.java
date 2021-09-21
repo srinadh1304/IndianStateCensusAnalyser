29da811b31053ebd5a7b947b235d7c2825374641
srinadh1304 <srinadh1304@gmail.com>
2021-09-21 21:08:35 +0530

    MODIFIED: testing if file format is incorrect

--- lib/src/test/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyserTest.java
@@ -8,9 +8,10 @@ public class CensusAnalyserTest
 {
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
 	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
-  	
+	private static final String INCORRECT_FILE_FORMAT = "./src/test/resources/CensusDataInWrongFormat.txt";
+	
   	@Test
-    public void givenStateCensusCSVFile_ShouldReturn_CorrectNumberOfRecords() 
+    public void givenStateCensusCSVFile_ShouldReturn_NumberOfRecords() 
     {
         try 
         {
@@ -24,7 +25,7 @@ public class CensusAnalyserTest
         }
     }
   	 @Test
- 	public void givenStateCensusCSVFile_WhenIncorrect_ShouldThrowException() 
+ 	public void givenStateCensusCSVFile_WithIncorrectPath_ShouldThrowException() 
      {
  		try 
  		{
@@ -36,8 +37,24 @@ public class CensusAnalyserTest
  		catch (CensusAnalyserException e) 
  		{
  			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
- 			e.printStackTrace();
+ 		
  		}
  	}
+  	@Test
+    public void givenStateCensusCSVFile_WithWrongFileFormat_ShouldThrowException() 
+    {
+		try 
+		{
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+ 			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.loadIndianCensusData(INCORRECT_FILE_FORMAT);
+		} 
+		catch (CensusAnalyserException e) 
+		{
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT, e.type);
+			e.printStackTrace();
+		}
+    }
     
 }
