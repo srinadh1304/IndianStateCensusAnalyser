55945c627af44c495b893cc04ab664b0dd617263
srinadh1304 <srinadh1304@gmail.com>
2021-09-21 21:14:26 +0530

    MODIFIED: testing file with incorrect delimiter

--- lib/src/test/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyserTest.java
@@ -9,45 +9,46 @@ public class CensusAnalyserTest
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
 	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 	private static final String INCORRECT_FILE_FORMAT = "./src/test/resources/CensusDataInWrongFormat.txt";
-	
-  	@Test
-    public void givenStateCensusCSVFile_ShouldReturn_NumberOfRecords() 
-    {
-        try 
-        {
-            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadIndianCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-            Assert.assertEquals(29,numOfRecords);
-        } 
-        catch (CensusAnalyserException e) 
-        { 
-        	e.printStackTrace();
-        }
-    }
-  	 @Test
- 	public void givenStateCensusCSVFile_WithIncorrectPath_ShouldThrowException() 
-     {
- 		try 
- 		{
- 			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
- 			ExpectedException exceptionRule = ExpectedException.none();
- 			exceptionRule.expect(CensusAnalyserException.class);
- 			censusAnalyser.loadIndianCensusData(WRONG_CSV_FILE_PATH);
- 		} 
- 		catch (CensusAnalyserException e) 
- 		{
- 			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
- 		
- 		}
- 	}
-  	@Test
-    public void givenStateCensusCSVFile_WithWrongFileFormat_ShouldThrowException() 
-    {
+	private static final String CSV_WITH_WRONG_DELIMITER = "./src/test/resources/CensusDataWithWrongDelimiter.csv";
+
+	@Test
+	public void givenStateCensusCSVFile_ShouldReturn_NumberOfRecords() 
+	{
 		try 
 		{
-            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            ExpectedException exceptionRule = ExpectedException.none();
- 			exceptionRule.expect(CensusAnalyserException.class);
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			int numOfRecords = censusAnalyser.loadIndianCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+			Assert.assertEquals(29,numOfRecords);
+		} 
+		catch (CensusAnalyserException e) 
+		{ 
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void givenStateCensusCSVFile_WithIncorrectPath_ShouldThrowException() 
+	{
+		try 
+		{
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.loadIndianCensusData(WRONG_CSV_FILE_PATH);
+		} 
+		catch (CensusAnalyserException e) 
+		{
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+
+		}
+	}
+	@Test
+	public void givenStateCensusCSVFile_WithWrongFileFormat_ShouldThrowException() 
+	{
+		try 
+		{
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
 			censusAnalyser.loadIndianCensusData(INCORRECT_FILE_FORMAT);
 		} 
 		catch (CensusAnalyserException e) 
@@ -55,6 +56,22 @@ public class CensusAnalyserTest
 			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT, e.type);
 			e.printStackTrace();
 		}
-    }
-    
+	}
+	@Test
+	public void givenStateCensusCSVFile_WhenCorrectButIncorrectDelimiter_ShouldThrowException() 
+	{
+		try 
+		{
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.loadIndianCensusData(CSV_WITH_WRONG_DELIMITER);
+		} 
+		catch (CensusAnalyserException e) 
+		{
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_WRONG_DELIMITER_OR_HEADER, e.type);
+			e.printStackTrace();
+		}
+	}
+
 }
