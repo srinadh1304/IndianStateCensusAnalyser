f8f871d49ab7539bfe4db1fa0cedd73ba6597857
srinadh1304 <srinadh1304@gmail.com>
2021-09-21 20:39:43 +0530

    ADDED: Custom exception class

---
+++ lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyserException.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.indianstatecensusanalyser;
+
+
+public class CensusAnalyserException extends Exception 
+{
+
+	enum ExceptionType 
+	{
+        CENSUS_FILE_PROBLEM, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER_OR_HEADER
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) 
+    {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) 
+    {
+        super(message, cause);
+        this.type = type;
+    }
+}
